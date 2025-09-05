e9b1a3ac869738ee8b79348603386b9f36231119
ivaann17 <83002576+ivaann17@users.noreply.github.com>
2025-09-05 12:11:23 +0200

    Add files via upload

---
+++ Denegación de Servicio/Segunda Prueba de Concepto/Página Web Maliciosa/script.php
@@ -0,0 +1,12 @@
+<!-- index.php -->
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <title>Prueba de Concepto - Denegación de Servicio</title>
+</head>
+<body>
+<h1>Prueba de Concepto - Denegación de Servicio</h1>
+  <h1>Atención Página Web Maliciosa</h1>
+</body>
+</html>
